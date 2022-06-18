9921210e080b398e030ae637d9861824c82638b4
unknown <maybex66@gmail.com>
2022-06-18 14:59:03 -0400

    Init

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import './style/style.scss'
+
+import React from 'react'
+
+function App() {
+  return (
+    <div className='main-container'>
+      <header></header>
+      <main></main>
+    </div>
+  );
+}
+
+export default App;
